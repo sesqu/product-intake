e48a06ddf59546b44e84d918c7b021ed32016bb4
sesqu <sesquu@gmail.com>
2026-09-26 21:33:50 +0200

    Stage runtime v36

--- enhancements.js
+++ enhancements.js
@@ -22,7 +22,7 @@
   const SecondaryViews = window.ProductIntakeSecondaryViews;
   const Navigation = window.ProductIntakeNavigation;
 
-  window.ProductIntakePWA?.setup({build:35,label:'v35'});
+  window.ProductIntakePWA?.setup({build:36,label:'v36'});
 
   function getProducts() {
     return IntakeSession.getProducts();
