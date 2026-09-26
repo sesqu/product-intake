c270f3baefae1672508ac55f0aa6b2c7f2c5b3e3
sesqu <sesquu@gmail.com>
2026-09-26 18:09:42 +0200

    Bump PWA cache for live cloud sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='product-intake-v10';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./product-storage.js?v=1','./enhancements.js?v=10'];
+const CACHE='product-intake-v11';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./product-storage.js?v=1','./enhancements.js?v=11'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
