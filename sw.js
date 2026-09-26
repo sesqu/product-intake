672f73ae2733d38f2640a88d5256b45a242b01df
sesqu <sesquu@gmail.com>
2026-09-26 19:35:46 +0200

    Bump PWA cache for iPhone layout v18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='product-intake-v17';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./product-storage.js?v=2','./enhancements.js?v=17'];
+const CACHE='product-intake-v18';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./product-storage.js?v=2','./enhancements.js?v=18'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
