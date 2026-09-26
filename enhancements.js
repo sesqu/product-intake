93af9f1aaae196aef0846cd9c2b28138e2b7ed71
sesqu <sesquu@gmail.com>
2026-09-26 21:02:57 +0200

    Publish modal modularization runtime v30

--- enhancements.js
+++ enhancements.js
@@ -12,7 +12,7 @@
   let photoData = [];
   const CatalogLookup = window.ProductIntakeCatalogLookup;
 
-  window.ProductIntakePWA?.setup({build:29,label:'v29'});
+  window.ProductIntakePWA?.setup({build:30,label:'v30'});
 
 
   const CloudProducts = window.ProductIntakeCloudProducts;
